7475af3267c3ab5ed982838b7b509d804bc8cced
ouchen <m.ouchen@lynx-business.com>
2025-06-27 12:40:55 +0200

    feat(number-field): add NumberField component and related subcomponents for enhanced input handling

---
+++ resources/js/components/ui/custom/input/NumberInput.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import {
+    NumberField,
+    NumberFieldContent,
+    NumberFieldDecrement,
+    NumberFieldIncrement,
+    NumberFieldInput,
+} from '@/components/ui/number-field';
+import { NumberFieldRootEmits, NumberFieldRootProps, useForwardPropsEmits } from 'reka-ui';
+
+type Props = NumberFieldRootProps;
+const props = defineProps<Props>();
+const emits = defineEmits<NumberFieldRootEmits>();
+
+const forwarded = useForwardPropsEmits(props, emits);
+</script>
+<template>
+    <NumberField v-bind="forwarded">
+        <NumberFieldContent>
+            <NumberFieldDecrement
+                class="text-muted-foreground pointer-events-none text-xs !opacity-100"
+                v-if="$slots.start"
+            >
+                <slot name="start" />
+            </NumberFieldDecrement>
+            <NumberFieldInput />
+            <NumberFieldIncrement
+                class="text-muted-foreground pointer-events-none text-xs !opacity-100"
+                v-if="$slots.end"
+            >
+                <slot name="end" />
+            </NumberFieldIncrement>
+        </NumberFieldContent>
+    </NumberField>
+</template>
